32a63e67e1bb002b40ac1cda87e1a7d30803f95b
karansinghal8395@gmail.com <you@example.com>
2023-02-22 13:42:17 +0530

    Initial Commit

--- force-app/main/default/lwc/quoteTotalSummary/quoteTotalSummary.js
+++
@@ -1,8 +0,0 @@
-/*
- * Provus Services Quoting
- * Copyright (c) 2023 Provus Inc. All rights reserved.
- */
-
-import { LightningElement } from "lwc";
-
-export default class QuoteTotalSummary extends LightningElement {}
